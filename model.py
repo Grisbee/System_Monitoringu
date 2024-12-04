17b0c873e0c5d2c76afb953e1103dde8a6706c7c
Grisbee <pawel.sliwkaa1@gmail.com>
2024-12-04 22:27:42 +0100

    added skeleton for model and presenter

---
+++ model.py
@@ -0,0 +1,295 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+# Interfejs iModel
+class IModel(ABC):
+    @abstractmethod
+    def get_stan_alarmu(self) -> bool:
+        pass
+
+    @abstractmethod
+    def get_tryb(self) -> str:
+        pass
+
+    @abstractmethod
+    def set_stan_alarmu(self, stan: bool) -> None:
+        pass
+
+    @abstractmethod
+    def set_tryb(self, tryb: str) -> None:
+        pass
+
+    @abstractmethod
+    def getRFID(self) -> str:
+        pass
+
+    @abstractmethod
+    def getDaneLogowania(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_kod_czujnika(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_rodzaj_czujnika(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_pomieszczenie_czujnika(self) -> str:
+        pass
+
+    @abstractmethod
+    def set_kod_czujnika(self, kod: int) -> None:
+        pass
+
+    @abstractmethod
+    def set_rodzaj_czujnika(self, rodzaj: str) -> None:
+        pass
+
+    @abstractmethod
+    def set_pomieszczenie_czujnika(self, pomieszczenie: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_kod_kamery(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_pomieszczenie_kamery(self) -> str:
+        pass
+
+    @abstractmethod
+    def set_kod_kamery(self, kod: int) -> None:
+        pass
+
+    @abstractmethod
+    def set_pomieszczenie_kamery(self, pomieszczenie: str) -> None:
+        pass
+
+    @abstractmethod
+    def update_haslo(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_login(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_haslo(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_rola(self) -> str:
+        pass
+
+
+# Klasa Alarm
+class Alarm:
+    def __init__(self, stan: bool, tryb: str):
+        self.stan = stan
+        self.tryb = tryb
+
+    def get_stan_alarmu(self) -> bool:
+        pass
+
+    def set_stan_alarmu(self, stan: bool) -> None:
+        pass
+
+    def get_tryb(self) -> str:
+        pass
+
+    def set_tryb(self, tryb: str) -> None:
+        pass
+
+
+# Klasa Czujnik
+class Czujnik:
+    def __init__(self, kod: int, typ: str, pomieszczenie: str):
+        self.kod = kod
+        self.typ = typ
+        self.pomieszczenie = pomieszczenie
+
+    def get_kod_czujnika(self) -> int:
+        pass
+
+    def get_rodzaj_czujnika(self) -> str:
+        pass
+
+    def get_pomieszczenie_czujnika(self) -> str:
+        pass
+
+    def set_kod_czujnika(self, kod: int) -> None:
+        pass
+
+    def set_rodzaj_czujnika(self, rodzaj: str) -> None:
+        pass
+
+    def set_pomieszczenie_czujnika(self, pomieszczenie: str) -> None:
+        pass
+
+
+# Klasa Kamera
+class Kamera:
+    def __init__(self, kod: int, pomieszczenie: str):
+        self.kod = kod
+        self.pomieszczenie = pomieszczenie
+
+    def get_pomieszczenie_kamery(self) -> str:
+        pass
+
+    def get_kod_kamery(self) -> int:
+        pass
+
+    def set_pomieszczenie_kamery(self, pomieszczenie: str) -> None:
+        pass
+
+    def set_kod_kamery(self, kod: int) -> None:
+        pass
+
+
+# Klasa Uzytkownik
+class Uzytkownik:
+    def __init__(self, login: str, haslo: str, rola: str):
+        self.login = login
+        self.haslo = haslo
+        self.rola = rola
+
+    def update_haslo(self) -> None:
+        pass
+
+    def get_login(self) -> str:
+        pass
+
+    def get_haslo(self) -> str:
+        pass
+
+    def get_rola(self) -> str:
+        pass
+
+
+# Interfejs iDao
+class IDao(ABC):
+    @abstractmethod
+    def get(self):
+        pass
+
+    @abstractmethod
+    def delete(self, obj):
+        pass
+
+    @abstractmethod
+    def getAll(self) -> List:
+        pass
+
+    @abstractmethod
+    def update(self, obj):
+        pass
+
+
+# Klasa DaoCzujnik implementująca iDao
+class DaoCzujnik(IDao):
+    def get(self) -> Czujnik:
+        pass
+
+    def delete(self, czujnik: Czujnik) -> None:
+        pass
+
+    def getAll(self) -> List[Czujnik]:
+        pass
+
+    def update(self, czujnik: Czujnik) -> None:
+        pass
+
+
+# Klasa DaoKamera implementująca iDao
+class DaoKamera(IDao):
+    def get(self) -> Kamera:
+        pass
+
+    def delete(self, kamera: Kamera) -> None:
+        pass
+
+    def getAll(self) -> List[Kamera]:
+        pass
+
+    def update(self, kamera: Kamera) -> None:
+        pass
+
+
+# Klasa DaoUzytkownik implementująca iDao
+class DaoUzytkownik(IDao):
+    def get(self) -> Uzytkownik:
+        pass
+
+    def delete(self, uzytkownik: Uzytkownik) -> None:
+        pass
+
+    def getAll(self) -> List[Uzytkownik]:
+        pass
+
+    def update(self, uzytkownik: Uzytkownik) -> None:
+        pass
+
+
+# Klasa Fasada implementująca IModel
+class Fasada(IModel):
+    def get_stan_alarmu(self) -> bool:
+        pass
+
+    def get_tryb(self) -> str:
+        pass
+
+    def set_stan_alarmu(self, stan: bool) -> None:
+        pass
+
+    def set_tryb(self, tryb: str) -> None:
+        pass
+
+    def getRFID(self) -> str:
+        pass
+
+    def getDaneLogowania(self) -> str:
+        pass
+
+    def get_kod_czujnika(self) -> int:
+        pass
+
+    def get_rodzaj_czujnika(self) -> str:
+        pass
+
+    def get_pomieszczenie_czujnika(self) -> str:
+        pass
+
+    def set_kod_czujnika(self, kod: int) -> None:
+        pass
+
+    def set_rodzaj_czujnika(self, rodzaj: str) -> None:
+        pass
+
+    def set_pomieszczenie_czujnika(self, pomieszczenie: str) -> None:
+        pass
+
+    def get_kod_kamery(self) -> int:
+        pass
+
+    def get_pomieszczenie_kamery(self) -> str:
+        pass
+
+    def set_kod_kamery(self, kod: int) -> None:
+        pass
+
+    def set_pomieszczenie_kamery(self, pomieszczenie: str) -> None:
+        pass
+
+    def update_haslo(self) -> None:
+        pass
+
+    def get_login(self) -> str:
+        pass
+
+    def get_haslo(self) -> str:
+        pass
+
+    def get_rola(self) -> str:
+        pass
---
+++ presenter.py
@@ -0,0 +1,192 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+# Interfejs iModel
+class IModel(ABC):
+    @abstractmethod
+    def get_stan_alarmu(self) -> bool:
+        pass
+
+    @abstractmethod
+    def get_tryb(self) -> str:
+        pass
+
+    @abstractmethod
+    def set_stan_alarmu(self, stan: bool) -> None:
+        pass
+
+    @abstractmethod
+    def set_tryb(self, tryb: str) -> None:
+        pass
+
+    @abstractmethod
+    def getRFID(self) -> str:
+        pass
+
+    @abstractmethod
+    def getDaneLogowania(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_kod_czujnika(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_rodzaj_czujnika(self) -> str:
+        pass
+
+    @abstractmethod
+    def get_pomieszczenie_czujnika(self) -> str:
+        pass
+
+
+# Interfejs iPresenter
+class IPresenter(ABC):
+    @abstractmethod
+    def zmien_haslo(self, haslo: str, nowe_haslo: str) -> None:
+        pass
+
+    @abstractmethod
+    def login(self, login: str, haslo: str) -> None:
+        pass
+
+    @abstractmethod
+    def dodaj_czujnik(self, typ: str, kod: int) -> None:
+        pass
+
+    @abstractmethod
+    def kalibracja_czujnika(self, kod: int, parametry: List[int]) -> None:
+        pass
+
+    @abstractmethod
+    def usun_czujnik(self, kod: int) -> None:
+        pass
+
+    @abstractmethod
+    def wlacz_alarm(self) -> None:
+        pass
+
+    @abstractmethod
+    def wylacz_alarm(self) -> None:
+        pass
+
+    @abstractmethod
+    def aktywuj_czuwanie(self) -> None:
+        pass
+
+    @abstractmethod
+    def dezaktywuj_czuwanie(self) -> None:
+        pass
+
+    @abstractmethod
+    def autoryzacja_rfid(self, klucz: str) -> None:
+        pass
+
+
+# Klasa Fasada implementująca iPresenter
+class Fasada(IPresenter):
+    def aktywuj_czuwanie(self) -> None:
+        pass
+
+    def autoryzacja_rfid(self, klucz: str) -> None:
+        pass
+
+    def dezaktywuj_czuwanie(self) -> None:
+        pass
+
+    def dodaj_czujnik(self, typ: str, kod: int) -> None:
+        pass
+
+    def kalibracja_czujnika(self, kod: int, parametry: List[int]) -> None:
+        pass
+
+    def usun_czujnik(self, kod: int) -> None:
+        pass
+
+    def wlacz_alarm(self) -> None:
+        pass
+
+    def wylacz_alarm(self) -> None:
+        pass
+
+    def login(self, login: str, haslo: str) -> None:
+        pass
+
+    def zmien_haslo(self, haslo: str, nowe_haslo: str) -> None:
+        pass
+
+    def zarejestruj(self, login: str, haslo: str) -> None:
+        pass
+
+    def dodaj_kamere(self, kod: int, pomieszczenie: str) -> None:
+        pass
+
+    def usun_kamere(self, kamera) -> None:
+        pass
+
+
+# Klasa ObslugaAlarmu
+class ObslugaAlarmu:
+    def wlacz_alarm(self) -> None:
+        pass
+
+    def wylacz_alarm(self) -> None:
+        pass
+
+    def aktywuj_czuwanie(self) -> None:
+        pass
+
+    def dezaktywuj_czuwanie(self) -> None:
+        pass
+
+    def setTryb(self, tryb: str) -> None:
+        pass
+
+    def getTryb(self) -> str:
+        pass
+
+    def getStan(self) -> bool:
+        pass
+
+    def setStan(self, stan: bool) -> None:
+        pass
+
+
+# Klasa ObslugaKamer
+class ObslugaKamer:
+    def dodaj_kamere(self, kod: int, pomieszczenie: str) -> None:
+        pass
+
+    def usun_kamere(self, kamera) -> None:
+        pass
+
+
+# Klasa ObslugaCzujnikow
+class ObslugaCzujnikow:
+    def dodaj_czujnik(self, kod: int, typ: str, pomieszczenie: str):
+        pass
+
+    def kalibracja_czujnika(self, czujnik, parametry: List[int]):
+        pass
+
+    def usun_czujnik(self, czujnik):
+        pass
+
+
+# Klasa ObslugaLogowania
+class ObslugaLogowania:
+    def login(self, login: str, haslo: str) -> None:
+        pass
+
+    def zmien_haslo(self, haslo: str, nowe_haslo: str) -> None:
+        pass
+
+    def zarejestruj(self, login: str, haslo: str) -> None:
+        pass
+
+
+# Klasa AutoryzacjaRFID
+class AutoryzacjaRFID:
+    def autoryzacja_rfid(self, klucz: str, uzytkownik) -> bool:
+        pass
