089f52dd1f713fa2e6b59ef3468701082378634a
Paweł Śliwka <pawelsliwka@Pawes-MacBook-Air.local>
2025-01-22 13:13:22 +0100

    ordered

--- tests/zad3.py
+++ tests/zad3.py
@@ -5,9 +5,24 @@ from model.Kamera import Kamera
 from model.Czujnik import Czujnik
 
 
+def ordered_test_loader():
+    loader = unittest.TestLoader()
+    loader.sortTestMethodsUsing = lambda x, y: {
+        'test_dao_1': 0,
+        'test_dao_kamery_parameterized': 1,
+        'test_dao_czujniki_parameterized': 2
+    }[x] - {
+        'test_dao_1': 0,
+        'test_dao_kamery_parameterized': 1,
+        'test_dao_czujniki_parameterized': 2
+    }[y]
+    return loader
+
+
 class TestDao(unittest.TestCase):
     def setUp(self):
         self.dao = Dao()
+
     def test_dao_1(self):
         self.dao.update_kamera(Kamera(30, "salon"))
         self.assertEqual(self.dao.get_kamery()[-1].get_pomieszczenie(), "salon")
@@ -17,7 +32,7 @@ class TestDao(unittest.TestCase):
         (10, "korytarz", 30, "salon"),
         (120, "lobby", 30, "salon"),
     ])
-    def test_dao_parameterized(self, id, nazwa, expected_result1, expected_result2):
+    def test_dao_kamery_parameterized(self, id, nazwa, expected_result1, expected_result2):
         self.dao.update_kamera(Kamera(id, nazwa))
         kamera = self.dao.get_kamery()[-1]
         self.dao.delete_kamera(kamera)
@@ -42,3 +57,9 @@ class TestDao(unittest.TestCase):
         self.assertEqual(result1, expected_result1)
         self.assertEqual(result2, expected_result2)
         self.assertEqual(result3, expected_result3)
+
+
+if __name__ == '__main__':
+    suite = ordered_test_loader().loadTestsFromTestCase(TestDao)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
